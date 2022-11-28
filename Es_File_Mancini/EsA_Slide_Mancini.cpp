1f8ffb9a12d1a7558d5d71caa15f23fddbd563af
Mancio <113896978+Mancini-Matteo@users.noreply.github.com>
2022-11-28 14:40:12 +0100

    Update EsA_Slide_Mancini.cpp

--- Es_File_Mancini/EsA_Slide_Mancini.cpp
+++ Es_File_Mancini/EsA_Slide_Mancini.cpp
@@ -8,30 +8,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAIUSC -32
 
 int main()
 {
-	char nomeFile1[] ={"nomi.txt"};
-	char nomeFile2[]={"NOMI2.txt"};			
+	char nomeFile1[] ={"nomi.txt"}; //File di partenza
+	char nomeFile2[]={"NOMI2.txt"}; //File da creare			
 	char c;
 
-	FILE *puntaFile1,*puntaFile2;						
+	FILE *puntaFile1,*puntaFile2; //Puntatori ai file					
 	
-	puntaFile1=fopen(nomeFile1,"r");			
-	puntaFile2=fopen(nomeFile2,"w");				
+	puntaFile1=fopen(nomeFile1,"r"); //Apertura "nomi.txt" in modalità read			
+	puntaFile2=fopen(nomeFile2,"w"); //Apertura "NOMI2.txt" in modalità write				
 	
-	if(puntaFile1!=0 && puntaFile2!=0)					
+	if(puntaFile1!=0 && puntaFile2!=0) //Controllo se il file esiste ed è stato aperto correttamente					
 	{
-		while(!feof(puntaFile1))                   
+		while(!feof(puntaFile1)) //Ciclo che verifica quando il file finisce                   
 		{
 			c=fgetc(puntaFile1);					
-			if(c>='a'&&c<='z')
-				c-=32;
-			fputc(c,puntaFile2);
+			if(c>='a'&&c<='z') //Controllo se le lettere nel file sono minuscole
+				c-=MAIUSC; //Sottraggo 32 al numero del codice asii corrispondente alla lettera minuscola per trasformarla in maiuscola
+			fputc(c,puntaFile2); //Scrittura lettere in maiuscoilo nel fil "NOMI2.txt"
 		}
-		fclose(puntaFile1);
-		fclose(puntaFile2);
+		fclose(puntaFile1); //Chiusura file "nomi.txt"
+		fclose(puntaFile2); //Chiusura file "NOMI2.txt"
 	}
 	else
-		printf("\nIL FILE NON PUO' ESSERE APERTO!");
+		printf("\nIL FILE NON PUO' ESSERE APERTO!"); //Errore apertura file 
 }

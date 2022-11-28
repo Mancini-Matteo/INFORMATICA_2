60866f8e56ee46c829c8b1287393cad52a36a14c
Mancio <113896978+Mancini-Matteo@users.noreply.github.com>
2022-11-28 14:45:25 +0100

    Update EsB_Slide_Mancini.cpp

--- Es_File_Mancini/EsB_Slide_Mancini.cpp
+++ Es_File_Mancini/EsB_Slide_Mancini.cpp
@@ -11,26 +11,26 @@
 
 int main()
 {
-	char nomeFile1[] ={"inTXT.txt"};
-	char nomeFile2[]={"outTXT.txt"};
+	char nomeFile1[] ={"inTXT.txt"}; //Fle di partenza 
+	char nomeFile2[]={"outTXT.txt"}; //File in cui copiare il testo di "inTXT.txt"
 	int err;
 	char c;
 	
-	FILE *puntaFile1,*puntaFile2;
+	FILE *puntaFile1,*puntaFile2; //Puntatori ai file
 	
-	puntaFile1=fopen(nomeFile1,"r");
-	puntaFile2=fopen(nomeFile2,"w");
+	puntaFile1=fopen(nomeFile1,"r"); //Apertura file "inTXT.txt" in modalità read
+	puntaFile2=fopen(nomeFile2,"w"); //Apertura file "outTXT.txt" in modalità write
 	
-	if(puntaFile1!=0 && puntaFile2!=0)
+	if(puntaFile1!=0 && puntaFile2!=0) //Controllo se il file esiste ed è stato aperto correttamente
 	{
-		while(!feof(puntaFile1))
+		while(!feof(puntaFile1)) //Ciclo che verifica la fine del file
 		{
-			c=fgetc(puntaFile1);
-			fputc(c,puntaFile2);
+			c=fgetc(puntaFile1); 
+			fputc(c,puntaFile2); //Copio i caratteri del primo file nel secondo
 		}
-		fclose(puntaFile1);
+		fclose(puntaFile1); //Chiusura file
 		fclose(puntaFile2);
 	}
 	else
-		printf("\nIL FILE NON PUO' ESSERE APERTO!");
+		printf("\nIL FILE NON PUO' ESSERE APERTO!"); //Errore apertura file
 }

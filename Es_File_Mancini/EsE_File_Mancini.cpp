dd35f3a0b283e0682353511b397ef570086abe32
Mancio <113896978+Mancini-Matteo@users.noreply.github.com>
2022-11-28 15:13:35 +0100

    Update EsE_File_Mancini.cpp

--- Es_File_Mancini/EsE_File_Mancini.cpp
+++ Es_File_Mancini/EsE_File_Mancini.cpp
@@ -1,133 +1,125 @@
-/*****************
-* Matteo Mancini *
-*      4^H       *
-*    28/11/22    *
-*   Esercizio E  *
-*   Versione 1.5 *
-******************/
+/** ****************************************************************************************
+* \mainpage Esercizio E file di testo
+*
+* @brief Partendo da una parola in un file di testo viene cryptata con l'algoritmo di cesare
+* File usati: "testo.txt", "crypt.txt", "decrypt.txt"
+* 
+* @author Matteo Mancini
+* @date 28/11/22
+* @version 1.0 <14/11/22> Creazione del programma
+* @version 1.1 <15/11/22> Modifica funzioni
+* @version 1.2 <17/11/22> Implementazione commenti
+* @version 1.3 <28/11/22> Rimozione switch case
+* @version 1.4 <28/11/22> Fine scrittura programma
+*/
 
 #include <stdio.h>
+#include <stdlib.h>
+#define cryptXYZ -23
 
-void crypt(char testo []);
-void decrypt(char testo []);
+void cesareCrypt(char [],char []);
+void cesareDecrypt(char [],char []);
 
 int main()
 {
-	
-	char nomeFile1[]={"text.txt"};
+// Assegnazione ad ogni stringa il nome del file
+	char nomeFile1[]={"testo.txt"};
 	char nomeFile2[]={"crypt.txt"};
-	crypt(nomeFile1);
-	decrypt(nomeFile2);
+	char nomeFile3[]={"decrypt.txt"};
+ 
+	cesareCrypt(nomeFile1,nomeFile2);
+	cesareDecrypt(nomeFile2,nomeFile3);
+
+}
+
+void cesareCrypt(char nomeFile1[],char nomeFile2[])
+{
+	char c;					
+	FILE *FileIN,*FileOUT; 		//dichiara i puntatori
+	
+	FileIN=fopen(nomeFile1,"r"); 		//apre il file iniziale in read (lettura)
+	
+	FileOUT=fopen(nomeFile2,"w");		//apre il file di uscita in write (scritura)
+	
+	if (FileIN != NULL && FileOUT != NULL)  //controlla se il puntatore è diverso da NULL 
+	{
+		while(!feof(FileIN))
+		{
+		
+			c=fgetc(FileIN);			//c prende il carattere dal file iniziale
+			
+			if(c==' ')	
+			{
+				c=' ';
+				fputc(c,FileOUT);
+			}
+			else
+			{
+				if((c>='A' && c<='Z') || (c>='a' && c<='z'))  //controllo se il carattere è presente nell'alfabeto
+				{
+							
+					if(c=='X' || c=='Y' || c=='Z' || c=='x' || c=='y' || c=='z')  //controllo se il carattere è x,y,z MAIUSC o MINUSC
+					{
+						c-=cryptXYZ;			//sottrae a c 23 al valore del carattere del codice ascii
+					}
+					fputc(c,FileOUT);		//inserisce il carattere preso da c nel file destinazione	
+				}
+			}			
+		}
+	}
+
+	else
+	{
+		printf("\nerrore in apertura file");
+		printf("\n");
+	}
+		
+	fclose(FileIN);			//chiude file iniziale
+	fclose(FileOUT);		//chiude file destinazione
 	
 }
 
-
-void crypt(char nomeFile1[])
+void cesareDecrypt(char nomeFile1[],char nomeFile2[])
 {
-	char s;
-	FILE *puntaFile1,*puntaFile2;//puntatori ai file
-	puntaFile1=fopen(testo,"r");//apertura primo file in modalità read
-	puntaFile2=fopen("crypt.txt","a");//apertura file crypt in modalità append
+	char c;
+	FILE *FileIN,*FileOUT;
 	
-	if(puntaFile1!=0 && puntaFile2!=0)
+	FileIN=fopen(nomeFile1,"r");
+	FileOUT=fopen(nomeFile2,"w");
+	
+	if (FileIN != NULL && FileOUT != NULL)
 	{
-		while(!feof(puntaFile1)) // ciclo che verifica la fine del file
+		while(!feof(FileIN))
 		{
-			s=fgetc(puntaFile1); 
 		
-			if(s>='a'&& s<='w'|| s>='A'&& s<='W')//controllo  carattere sia nell'alfabeto
+			c=fgetc(FileIN);
+			
+			if(c==' ')
 			{
-				s+=3;
-				fputc(s,puntaFile2);	//mette il carattere nel nuovo file		
+				c=' ';
+				fputc(c,FileOUT);
 			}
-			else 
+			else
 			{
-				switch (s)
+				if((c>='A' && c<'Z') || (c>='a' && c<'z'))
 				{
-					case 88: s=97;
-					break;
+							
+					if(c=='A' || c=='B' || c=='C' || c=='a' || c=='b' || c=='c')  //controlla se c uguale a x,y, e z maiuscole e minuscole
+					{
+						c+=26;			//aggiunge a c 26 nel codice ascii
+					}
 					
-					case 89: s=98;
-					break;
-					
-					case 90: s=99;
-					break;
-					
-					case 120: s=65;
-					break;
-					
-					case 121: s=66;
-					break;
-					
-					case 122: s=67;
-					break;					
+					c-=3;			//toglie a c 3 nel codice ascii
+
+				
+					fputc(c,FileOUT);		//inserisce il carattere preso da c nel file destinazione
 				}
-				fputc(s,p1);//mette il carattere nel nuovo file	
-			}	
+			}
 		}
 	}
-	else 
+	else
 	{
-		printf("\nIL FILE NON PUO' ESSERE APERTO!"); // errore apertura file
+		printf("\nerrore in apertura file");
+		printf("\n");
 	}
-	fclose(puntaFile1);
-	fclose(puntaFile2);
-}
-
-void decrypt(char nomeFile1[])
-{
-	char s;
-	FILE *puntaFile1,*puntaFile2;//puntatori ai file
-	puntaFile1=fopen(testo,"r");//apertura primo file in modalità read
-	puntaFile2=fopen("decrypt.txt","a");//apertura file crypt in modalità append
-	
-	if(puntaFile1!=0&&puntaFile2!=0)//controllo se i file si sono aperti senza problemi
-	{
-		while(!feof(puntaFile1))//ciclo fino a quando non finisce il file
-		{
-			s=fgetc(puntaFile1);  //prende ilo carattere
-		
-			if((s>='d'&& s<='z')||(s>='D'&& s<='Z')) //controllo  carattere sia nell'alfabeto
-			{
-				s-=3;
-				fputc(s,puntaFile2);	//mette il carattere nel nuovo file		
-			}
-			else 
-			{
-				switch (s)
-				{
-					case 97: 
-						s=88;
-					break;
-					
-					case 98:
-						s=89;
-					break;
-					
-					case 99:
-						s=90;
-					break;
-					
-					case 65: 
-						s=120;
-					break;
-					
-					case 66: 
-						s=121;
-					break;
-					
-					case 67: 
-						s=122;
-					break;					
-				}
-				fputc(s,puntaFile2);//mette il carattere nel nuovo file	
-			}	
-		}
-	}
-	else 
-	{
-		printf("\nIL FILE NON PUO' ESSERE APERTO!"); // errore apertura file
-	}
-	fclose(puntaFile1);
-	fclose(puntaFile2);
 }
